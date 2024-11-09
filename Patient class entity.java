3436c67397c47a0d28d7e9ffeac0e43269448783
Prasntu21 <167998144+Prasntu21@users.noreply.github.com>
2024-11-09 16:11:28 +0800

    Add files via upload

---
+++ Patient class entity.java
@@ -0,0 +1,46 @@
+package entities;
+
+public abstract class Patient {
+    private String patientID;
+    private String patientPass;
+    private boolean firstLogin;
+
+    public Patient(String patientID, String patientPass, boolean firstLogin) {
+        this.patientID = patientID;
+        this.patientPass = patientPass;
+        this.firstLogin = firstLogin;
+    }
+
+    
+    public String getPatientID() {
+        return this.patientID;
+    }
+
+    public boolean validatePassword(String patientPass) {
+        return this.patientPass.equals(patientPass);
+    }
+
+    public boolean isFirstLogin() {
+        return this.firstLogin;
+    }
+
+    
+    public void changePassword(String newPatientPass) {
+        this.firstLogin = false;
+        this.patientPass = newPatientPass;
+    }
+
+    public void resetPassword() {
+        this.firstLogin = true;
+        this.patientPass = "defaultPassword";
+    }
+
+    @Override
+    public String toString() {
+        return "Patient {" +
+                "patientID='" + patientID + '\'' +
+                ", firstLogin=" + firstLogin +
+                '}';
+    }
+}
+
---
+++ User.java
@@ -0,0 +1,36 @@
+package entities;
+
+public abstract class User {
+	private String userID;
+	private String userPass;
+	private boolean firstLogin;
+	
+	public User(String userID, String userPass, boolean firstLogin) {
+		this.userID = userID;
+		this.userPass = userPass;
+		this.firstLogin = firstLogin;
+	}
+	
+	public String getUserID() {
+		return this.userID;
+	}
+	
+	public boolean validatePassword(String userPass) {
+		if(this.userPass.equals(userPass)) return true;
+		return false;
+	}
+	
+	public boolean isFirstLogin() {
+		return this.firstLogin;
+	}
+	
+	public void changePassword(String newUserPass) {
+		this.firstLogin = false;
+		this.userPass = newUserPass;
+	}
+	
+	public void resetPassword() {
+		this.firstLogin = true;
+		this.userPass = "password";
+	}
+}
