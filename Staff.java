a0404cfb21bbcddc3f614cbef6758185a92e7690
Prasntu21 <167998144+Prasntu21@users.noreply.github.com>
2024-11-09 16:41:56 +0800

    Staff.java

--- Staff.java
+++ Staff.java
@@ -1,38 +1,13 @@
 package entities;
 
-public abstract class Staff {
-    private String staffID;
-    private String staffPass;
-    private boolean firstLogin;
+public class Staff extends User{
 
     public Staff(String staffID, String staffPass, boolean firstLogin) {
-        this.staffID = staffID;
-        this.staffPass = staffPass;
-        this.firstLogin = firstLogin;
+        super(staffID, staffPass, firstLogin)
     }
 
-    // Getters
     public String getStaffID() {
-        return this.staffID;
-    }
-
-    public boolean validatePassword(String staffPass) {
-        return this.staffPass.equals(staffPass);
-    }
-
-    public boolean isFirstLogin() {
-        return this.firstLogin;
-    }
-
-    // Setters
-    public void changePassword(String newStaffPass) {
-        this.firstLogin = false;
-        this.staffPass = newStaffPass;
-    }
-
-    public void resetPassword() {
-        this.firstLogin = true;
-        this.staffPass = "defaultPassword";
+        return this.getID();
     }
 
     @Override
